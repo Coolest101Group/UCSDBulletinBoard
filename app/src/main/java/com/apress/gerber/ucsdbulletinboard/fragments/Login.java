59e78b366868a8f8fb84fa87fbbc977b0954b54e
vincent <no.root.toe@gmail.com>
2015-11-01 15:48:45 -0800

    added the firebase library so we can do databse stuff

--- app/src/main/java/com/apress/gerber/ucsdbulletinboard/fragments/Login.java
+++ app/src/main/java/com/apress/gerber/ucsdbulletinboard/fragments/Login.java
@@ -7,7 +7,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.apress.gerber.ucsdbulletinboard.MainActivity;
 import com.apress.gerber.ucsdbulletinboard.R;
+import com.firebase.client.Firebase;
 
 /**
  * Created by danielmartin on 10/23/15.
@@ -16,6 +18,14 @@ public class Login extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        //trying out writing to firebase db
+
+        //pull the database object from main activity
+        Firebase databaseRef = ((MainActivity)getActivity()).getdb();
+
+        //sending a test message to see if the database is receiving data
+        databaseRef.child("message").setValue("Hey I clicked login! Am I sending data?");
 
         View mV = inflater.inflate(R.layout.fragment_login, container, false);
         return mV;
