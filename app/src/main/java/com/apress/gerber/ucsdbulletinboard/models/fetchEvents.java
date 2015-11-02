17143b32d81b89a095cbda32fb0efc8f0260d793
Coolest101Group <no.root.toe@gmail.com>
2015-11-02 11:25:18 -0800

    implemented method for posting an event to a db --Daniel

--- app/src/main/java/com/apress/gerber/ucsdbulletinboard/models/fetchEvents.java
+++ app/src/main/java/com/apress/gerber/ucsdbulletinboard/models/fetchEvents.java
@@ -2,10 +2,26 @@ package com.apress.gerber.ucsdbulletinboard.models;
 
 import com.firebase.client.Firebase;
 
+import java.util.Random;
+
 /**
  * Created by danielmartin on 11/2/15.
  */
 public class fetchEvents {
+
+    private class myEvent{
+        private String mEventName;
+        private String mEventTime;
+        private String mEventDesc;
+
+        public myEvent(){}
+
+        public myEvent(String mEN, String mET, String mED){
+            mEventName = mEN;
+            mEventTime = mET;
+            mEventDesc = mED;
+        }
+    }
 
     private Firebase db;
     String[] mEventNames;
@@ -31,5 +47,15 @@ public class fetchEvents {
 
     public String[] getMasterList(){
         return mMasterList;
+    }
+
+    public boolean postEvent(String eventName, String eventTime, String eventDesc){
+        Random randomGenerator = new Random();
+        int eventNumber = randomGenerator.nextInt();
+
+        myEvent event = new myEvent(eventName, eventTime, eventDesc);
+
+        db.child("users").child(String.valueOf(eventNumber)).setValue(event);
+        return true;
     }
 }
