5b9ab6c7fcae03913c4bcd09964a4b44dd07cd3f
Coolest101Group <no.root.toe@gmail.com>
2015-11-05 19:22:48 -0800

    Create Event form now gives option to Take a photo or select from gallery - Daniel

--- app/src/main/java/com/apress/gerber/ucsdbulletinboard/CreateEvent.java
+++ app/src/main/java/com/apress/gerber/ucsdbulletinboard/CreateEvent.java
@@ -1,7 +1,13 @@
 package com.apress.gerber.ucsdbulletinboard;
 
 import android.app.ActionBar;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -12,19 +18,27 @@ import android.view.Window;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.apress.gerber.ucsdbulletinboard.*;
 import com.apress.gerber.ucsdbulletinboard.fragments.FeaturedEvents;
 import com.apress.gerber.ucsdbulletinboard.models.manageEvents;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.concurrent.TimeUnit;
 
 public class CreateEvent extends AppCompatActivity {
 
+    public static final int CAMERA_REQUEST = 10;
+    public static final int GALLERY_PICK = 20;
     private EditText eTitleView;
     private EditText eDescView;
     private DatePicker ePicker;
+    private Bitmap eImage;
+    private ImageView imgEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +55,7 @@ public class CreateEvent extends AppCompatActivity {
         eTitleView = (EditText)findViewById(R.id.event_title);
         eDescView = (EditText)findViewById(R.id.event_description);
         ePicker = (DatePicker)findViewById(R.id.event_date);
+        imgEvent = (ImageView) findViewById(R.id.iViewPhoto);
 
         // When user clicks "create" button invoke 'addEvent()' method
         Button mCreateEventButton = (Button) findViewById(R.id.event_sign_up_button);
@@ -63,8 +78,6 @@ public class CreateEvent extends AppCompatActivity {
         eTitleView.setError(null);
         eDescView.setError(null);
 
-
-
         String mTitle = eTitleView.getText().toString();
         String mDesc = eDescView.getText().toString();
         int year = ePicker.getYear();
@@ -74,25 +87,77 @@ public class CreateEvent extends AppCompatActivity {
         String mDate = new StringBuilder().append(month +1)
                 .append("-").append(day).append("-").append(year)
                 .append(" ").toString();
-
         boolean success = MainActivity.mDB.postEvent(mTitle, mDate, mDesc, day, month, year);
         if(success){
-
-            this.runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(getApplicationContext(), "Event added successfully.", Toast.LENGTH_LONG).show();
-                }
-            });
-
-            Intent intent = new Intent(this, FeaturedEvents.class);
-            this.startActivity(intent);
-
+            //Intent intent = new Intent(this, FeaturedEvents.class);
+            //this.startActivity(intent);
+            Toast.makeText(getApplicationContext(), "Event added successfully.", Toast.LENGTH_LONG).show();
+            finish();
         }
         else{
             Toast.makeText(getApplicationContext(), "Error.", Toast.LENGTH_LONG).show();
         }
-
         return true;
     }
+
+    public void btnGetCameraPhotoClicked(View v){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, false);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            if (requestCode == CAMERA_REQUEST){
+                Bitmap data1 = (Bitmap) data.getExtras().get("data");
+                eImage = data1;
+                imgEvent.setImageBitmap(eImage);
+            }
+        }
+
+        if(resultCode == RESULT_OK){
+            if(requestCode == GALLERY_PICK){
+                // the address of image in SD Card
+                Uri imageUri = data.getData();
+
+                // declare stream to read img data from SD
+                InputStream inputStream;
+                try{
+                    inputStream = getContentResolver().openInputStream(imageUri);
+
+                    Bitmap data2 = BitmapFactory.decodeStream(inputStream);
+                    int nh = (int) ( data2.getHeight() * (512.0 / data2.getWidth()) );
+                    Bitmap scaled = Bitmap.createScaledBitmap(data2, 512, nh, true);
+                    eImage = scaled;
+                    imgEvent.setImageBitmap(eImage);
+                }
+                catch(FileNotFoundException e){
+                    Toast.makeText(this, "Failed to open.", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+
+    public void btnGetGalleryClicked(View v){
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK);
+
+        File pictureDirectory = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        String picDirPath = pictureDirectory.getPath();
+        Uri data = Uri.parse(picDirPath);
+
+        // Set data and type
+        galleryIntent.setDataAndType(data, "image/*");
+
+        startActivityForResult(galleryIntent, GALLERY_PICK);
+
+
+    }
+
+
+
+
+
 }
