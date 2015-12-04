fcf9b8591f10419504137ee6f84dfeeaff9bb517
Shant Simonian <ssimonia@ucsd.edu>
2015-12-03 23:52:50 -0800

    Set up test class to add new tests; check TODO

--- app/src/androidTest/java/com/apress/gerber/ucsdbulletinboard/ApplicationTest.java
+++ app/src/androidTest/java/com/apress/gerber/ucsdbulletinboard/ApplicationTest.java
@@ -2,12 +2,28 @@ package com.apress.gerber.ucsdbulletinboard;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
+import android.widget.TextView;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
+    private CreateEvent createEventActivity;
+
     public ApplicationTest() {
         super(Application.class);
     }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        createEventActivity = new CreateEvent();
+
+        //TODO: Set up test cases by using findViewById to instantiate activity views, and run tests to see if they're functioning properly.
+        //Example:
+        //createEventActivity.findViewById(R.id.name_mainMenu);
+        //Do something with the name textview to see if it works
+    }
+
 }
